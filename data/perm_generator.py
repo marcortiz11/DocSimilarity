9b413e3c9ade8436c2d60ec2b4fe8ef30720219b
Juan López <juanlopezm94@gmail.com>
2016-11-12 20:48:59 +0100

    Add permutations dataset and scripts

---
+++ data/perm_generator.py
@@ -0,0 +1,16 @@
+from random import shuffle
+
+outdir = "permutations"
+wordfile = "words.txt"
+
+f = open(wordfile, 'r')
+words = f.read().splitlines()
+f.close()
+
+for i in range(20):
+    filename = "permutation-" + str(i+1) + ".txt"
+    f = open(outdir + "/" + filename, 'w')
+    shuffle(words)
+    text = ' '.join(words)
+    f.write(text)
+    f.close()
---
+++ data/tweet_collector.py
@@ -0,0 +1,48 @@
+# Import the necessary package to process data in JSON format
+import re
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Import the necessary methods from "twitter" library
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+# Variables that contains the user credentials to access Twitter API
+#XXX Keys needed
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+# Initiate the connection to Twitter Streaming API
+twitter_stream = TwitterStream(auth=oauth)
+
+# Get a sample of the public data following through Twitter
+iterator = twitter_stream.statuses.sample(language="en")
+
+# Print each tweet in the stream to the screen
+# Here we set it to stop after getting 1000 tweets.
+# You don't have to set it to stop, but can continue running
+# the Twitter API to collect data for days or even longer.
+tweet_count = 150000
+for tweet in iterator:
+    tweet_count -= 1
+    # Twitter Python Tool wraps the data returned by Twitter
+    # as a TwitterDictResponse object.
+    # We convert it back to the JSON format to print/score
+    text = tweet["text"]
+    outdir = "tweets"
+    filename = "tweet-" + str(150000-tweet_count) + ".txt"
+    f = open(outdir + "/" + filename, 'w')
+    f.write(text)
+    f.close()
+    #print(json.dumps(tweet))
+
+    # The command below will do pretty printing for JSON data, try it out
+    # print json.dumps(tweet, indent=4)
+
+    if tweet_count <= 0:
+        break
